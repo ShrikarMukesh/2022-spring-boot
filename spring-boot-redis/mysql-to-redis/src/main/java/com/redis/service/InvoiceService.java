a85fa2cb53e1f3f1a6caa20305439240dde614fb
Shrikar Mukesh <mukesh.shrikar7@gmail.com>
2022-10-03 10:20:36 +0530

    redis labs

---
+++ spring-boot-redis/mysql-to-redis/src/main/java/com/redis/service/InvoiceService.java
@@ -0,0 +1,15 @@
+package com.redis.service;
+
+
+import com.redis.entity.Invoice;
+
+import java.util.List;
+
+public interface InvoiceService {
+
+    public Invoice saveInvoice(Invoice inv);
+    public Invoice updateInvoice(Invoice inv, Integer invId);
+    public void deleteInvoice(Integer invId);
+    public Invoice getOneInvoice(Integer invId);
+    public List<Invoice> getAllInvoices();
+}
